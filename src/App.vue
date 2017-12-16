56f2f72d6b17a0e00c093a1f64f6f2a269251c13
Andrius Rimkus <rimkusrimkus@gmail.com>
2017-12-16 13:39:50 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="app">
+		<Counter v-for="(counter, $index) in counters"
+						 v-bind:key="$index"
+						 v-bind:initCount="counter.initCount">
+
+		<template slot="incPanel" slot-scope="props">
+			<span v-if="counter.type === 'text'"
+					v-on:click="props.inc">
+					Click me
+			</span>
+
+			<span v-if="counter.type === 'image'"
+					v-on:click="props.inc">
+					<img src="http://via.placeholder.com/50x50" />
+			</span>
+		</template>
+
+		<hr slot="footer" />
+
+		</Counter>
+	</div>
+</template>
+
+<script>
+	import Counter from './components/Counter.vue'
+
+	export default {
+    name: 'app',
+
+		data: function() {
+			return {
+				counters: [
+					{
+						initCount: 2,
+						type: 'image'
+					},
+					{
+						initCount: 5,
+						type: 'text'
+					}
+				]
+			}
+		},
+
+		components: {
+			Counter
+		}
+  }
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		text-align: center;
+		margin-top: 60px;
+	}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="counter-box">
+		{{ localCount }}
+		<slot name="incPanel" v-bind:inc="inc"></slot>
+		<slot name="footer"></slot>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	props: ['initCount'],
+
+	data: function() {
+		return {
+			localCount: 0
+		}
+	},
+
+	methods: {
+		inc: function() {
+			this.localCount++;
+		}
+	},
+
+	created: function() {
+		this.localCount = this.initCount;
+	}
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+	.counter-box {
+		display: inline-block;
+		margin: 0 10px;
+		padding: 10px;
+		border: 1px solid grey;
+	}
+
+</style>
